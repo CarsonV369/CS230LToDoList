493908a3712c38298474c739239966fa17f42af7
CarsonV369 <143527878+CarsonV369@users.noreply.github.com>
2023-11-29 23:52:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const taskInputRef = useRef(null);
+
+  const addTask = () => {
+    const taskText = taskInputRef.current.value.trim();
+
+    if (taskText === "") {
+      alert("Please enter a task!");
+      return;
+    }
+
+    setTasks(prevTasks => [...prevTasks, taskText]);
+    taskInputRef.current.value = "";
+  };
+
+  const removeTask = (index) => {
+    setTasks(prevTasks => prevTasks.filter((_, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Todo List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          ref={taskInputRef}
+          placeholder="Enter a new task"
+          className="task-input"
+        />
+        <button onClick={addTask} className="add-button">
+          Add Task
+        </button>
+      </div>
+
+      <ul className="todo-list">
+        {tasks.map((task, index) => (
+          <li
+            key={index}
+            className={index % 2 === 0 ? 'even-item' : 'odd-item'}
+          >
+            {task}
+            <button onClick={() => removeTask(index)} className="remove-button">
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
